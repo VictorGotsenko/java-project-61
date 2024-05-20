891e25cb4aae4eb25d29837629946b7259cb7871
VictorGotsenko <victor.gotsenko@gmail.com>
2024-05-20 14:25:41 +0500

    Add Gradles settings files

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("application")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("checkstyle")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+application {
+        mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
